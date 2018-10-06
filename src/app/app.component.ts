c37efe71b2faa2edc5877563ae9abd434c5447fc
Juan Tavares <juantavares2006@hotmail.com>
2018-10-06 03:49:52 -0300

    Contruindo uma melhor exibição do app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { PhotoService } from './photos/photo/photo.service';
+import { Photo } from './photos/photo/photo';
 
 @Component({
   selector: 'app-root',
@@ -8,15 +9,12 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent {
   title = 'Angular-APic';
-  photos = [{
-    url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg',
-    description: 'Leão'
-  }, {
-    url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Lioness_Etosha_NP.jpg/500px-Lioness_Etosha_NP.jpg',
-    description: 'Leoa'
-  }];
+  photos: Photo[] = [];
 
-  constructor(http: HttpClient) {
-    console.log(http);
+  constructor(photoService: PhotoService) {
+
+    photoService
+      .listFromUser('flavio')
+      .subscribe(photos => this.photos = photos);
   }
 }
