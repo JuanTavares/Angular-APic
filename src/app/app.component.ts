4f0a7dd2968ae86b85b0e894fcda522aef85b030
Juan Tavares <juantavares2006@hotmail.com>
2018-10-06 04:04:08 -0300

    Pequena refatoração no app.component
    Para seguir a padronização de projeto de OnInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PhotoService } from './photos/photo/photo.service';
 import { Photo } from './photos/photo/photo';
 
@@ -7,13 +7,15 @@ import { Photo } from './photos/photo/photo';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Angular-APic';
   photos: Photo[] = [];
 
-  constructor(photoService: PhotoService) {
+  constructor(private photoService: PhotoService) { }
 
-    photoService
+  ngOnInit() {
+
+    this.photoService
       .listFromUser('flavio')
       .subscribe(photos => this.photos = photos);
   }
