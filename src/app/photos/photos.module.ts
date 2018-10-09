9c89b5ebe3b70d485ea366b154a00ae1bbafcf51
Juan Tavares <juantavares2006@hotmail.com>
2018-10-08 22:15:45 -0300

    Criando componente de formulário de fotos
    Inicialmente apenas para usa-lo no sistema de rotas. Sua implementação efetiva virá mais adiante.

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -4,12 +4,13 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { PhotoComponent } from './photo/photo.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
+import { PhotoFormComponent } from './photo-form/photo-form.component';
 
 @NgModule({
   imports: [
     CommonModule,
     HttpClientModule
   ],
-  declarations: [PhotoComponent, PhotoListComponent]
+  declarations: [PhotoComponent, PhotoListComponent, PhotoFormComponent]
 })
 export class PhotosModule { }
