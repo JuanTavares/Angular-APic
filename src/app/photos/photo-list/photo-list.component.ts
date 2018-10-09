40ddf682bb0da11a98163c513f37b651b0cd0a50
Juan Tavares <juantavares2006@hotmail.com>
2018-10-08 21:46:01 -0300

    Criando photo-list.component

---
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+import { PhotoService } from '../photo/photo.service';
+import { Photo } from '../photo/photo';
+
+@Component({
+  selector: 'app-photo-list',
+  templateUrl: './photo-list.component.html',
+  styleUrls: ['./photo-list.component.css']
+})
+export class PhotoListComponent implements OnInit {
+
+
+}
