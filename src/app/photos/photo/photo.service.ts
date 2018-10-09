5210af0252dc12473929319b834592e8a9b87849
Juan Tavares <juantavares2006@hotmail.com>
2018-10-08 21:44:22 -0300

    Refatoração de organização

--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+
 import { Photo } from './photo';
 
 const API = "http://localhost:3000/";
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PhotoComponent } from './photo/photo.component';
 import { HttpClientModule } from '@angular/common/http';
+
+import { PhotoComponent } from './photo/photo.component';
+import { PhotoListComponent } from './photo-list/photo-list.component';
 
 @NgModule({
   imports: [
     CommonModule,
     HttpClientModule
   ],
-  declarations: [PhotoComponent],
-  exports: [PhotoComponent]
+  declarations: [PhotoComponent, PhotoListComponent]
 })
 export class PhotosModule { }
