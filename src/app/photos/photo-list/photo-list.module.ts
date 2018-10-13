3bbf47974a662487602e3011c5ad69c015253078
Juan Tavares <juantavares2006@hotmail.com>
2018-10-13 00:29:53 -0300

    Reorganizando todos os Modules

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PhotosComponent } from './photos/photos.component';
+import { LoadButtonComponent } from './load-button/load-button.component';
+import { FilterByDescriptionPipe } from './filter-by-description.pipe';
+import { PhotoListComponent } from './photo-list.component';
+import { PhotoModule } from '../photo/photo.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    PhotoModule
   ],
-  declarations: []
+  declarations: [
+    PhotosComponent,
+    LoadButtonComponent,
+    FilterByDescriptionPipe,
+    PhotoListComponent
+  ]
 })
 export class PhotoListModule { }
