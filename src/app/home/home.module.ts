67361d9768f20ea6c0aae42861a739e6aceaf020
Juan Tavares <juantavares2006@hotmail.com>
2018-10-18 18:52:15 -0300

    Importando e aplicando vmessage

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,11 +4,13 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
+import { VmessageModule } from '../shared/components/vmessage/vmessage.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    VmessageModule
   ],
   declarations: [SigninComponent, SignupComponent]
 })
