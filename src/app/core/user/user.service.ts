9996510b489453ead1a8ffaa145023f5c5ee141f
Juan Tavares <juantavares2006@hotmail.com>
2018-10-21 14:17:16 -0300

    Criando userService
    Para manter usuário autenticado

---
+++ src/app/core/user/user.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { TokenService } from '../token/token.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private tokenService: TokenService) { }
+
+  setToken(token: string) {
+    this.tokenService.setToken(token);
+  }
+}
