fcd9381a3519272b2f9ef239d5faf957bb981f65
Juan Tavares <juantavares2006@hotmail.com>
2018-10-22 20:24:43 -0300

    Criando regras de validação de formulário

--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  signupForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.formBuilder.group({
+      email: ['',
+        [
+          Validators.required,
+          Validators.email
+        ]
+      ],
+      fullName: ['',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(40)
+        ]
+      ],
+      userName: ['',
+        [
+          Validators.required,
+          Validators.pattern(/^[a-z0-9_\-]+$/),
+          Validators.minLength(2),
+          Validators.maxLength(40)
+        ]
+      ],
+      password: ['',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(18)
+        ]
+      ]
+    });
   }
 
 }
