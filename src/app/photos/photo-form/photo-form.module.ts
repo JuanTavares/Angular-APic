c48756691f71a0cbaedec80a0b3e0ac799b2bbc5
Juan Tavares <juantavares2006@hotmail.com>
2018-10-31 20:17:39 -0300

    Criando um pequeno formulário de submissão de novas photos

--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PhotoFormComponent } from './photo-form.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { VmessageModule } from 'src/app/shared/components/vmessage/vmessage.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    VmessageModule,
+    FormsModule
   ],
   declarations: [ PhotoFormComponent ]
 })
