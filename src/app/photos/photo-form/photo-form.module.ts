fa29a5bdafaea3005e793fda8c68819bc53e6d12
Juan Tavares <juantavares2006@hotmail.com>
2018-11-01 22:45:21 -0300

    Reutilizando módulo de photo para fazer preview de imagem antes do upload

--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -4,6 +4,7 @@ import { PhotoFormComponent } from './photo-form.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { VmessageModule } from 'src/app/shared/components/vmessage/vmessage.module';
 import { RouterModule } from '@angular/router';
+import { PhotoModule } from '../photo/photo.module';
 
 @NgModule({
   imports: [
@@ -11,7 +12,8 @@ import { RouterModule } from '@angular/router';
     ReactiveFormsModule,
     VmessageModule,
     FormsModule,
-    RouterModule
+    RouterModule,
+    PhotoModule
   ],
   declarations: [ PhotoFormComponent ]
 })
