690ffbe50e41b22e25a74ebda18c7dfd6f8da895
Juan Tavares <juantavares2006@hotmail.com>
2018-11-05 19:57:34 -0300

    Criando validação de formulário

--- src/app/photos/photo-details/photo-comments/photo-comments.component.ts
+++ src/app/photos/photo-details/photo-comments/photo-comments.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Observable } from 'rxjs';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 import { PhotoComments } from '../../photo/photo-comments';
 import { PhotoService } from '../../photo/photo.service';
 
@@ -11,13 +13,23 @@ import { PhotoService } from '../../photo/photo.service';
 export class PhotoCommentsComponent implements OnInit {
 
   @Input() photoId: number;
+  commentForm: FormGroup;
+
   comments$: Observable<PhotoComments[]>;
 
-  constructor(private photoService: PhotoService) { }
+  constructor(
+    private photoService: PhotoService,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
-
     this.comments$ = this.photoService.getComments(this.photoId);
+    this.commentForm = this.formBuilder.group({
+      comment: ['', Validators.compose([
+        Validators.required,
+        Validators.maxLength(300)
+      ])]
+    });
   }
 
 }
