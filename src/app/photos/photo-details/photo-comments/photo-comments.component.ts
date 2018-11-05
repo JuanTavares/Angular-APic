7d72fe891fe258c562360dc6a967a1937d05fbe6
Juan Tavares <juantavares2006@hotmail.com>
2018-11-05 19:11:47 -0300

    Criando componente para exibir comentários de photos

---
+++ src/app/photos/photo-details/photo-comments/photo-comments.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PhotoComments } from '../../photo/photo-comments';
+import { PhotoService } from '../../photo/photo.service';
+
+@Component({
+  selector: 'ap-photo-comments',
+  templateUrl: './photo-comments.component.html',
+  styleUrls: ['./photo-comments.component.css']
+})
+export class PhotoCommentsComponent implements OnInit {
+
+  @Input() photoId: number;
+  comments$: Observable<PhotoComments[]>;
+
+  constructor(private photoService: PhotoService) { }
+
+  ngOnInit() {
+
+    this.comments$ = this.photoService.getComments(this.photoId);
+  }
+
+}
--- src/app/photos/photo-details/photo-details.module.ts
+++ src/app/photos/photo-details/photo-details.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PhotoDetailsComponent } from './photo-details.component';
 import { PhotoModule } from '../photo/photo.module';
+import { PhotoCommentsComponent } from './photo-comments/photo-comments.component';
 
 @NgModule({
   imports: [
     CommonModule,
     PhotoModule
   ],
-  declarations: [PhotoDetailsComponent],
+  declarations: [PhotoDetailsComponent, PhotoCommentsComponent],
   exports: [PhotoDetailsComponent]
 })
 export class PhotoDetailsModule { }
