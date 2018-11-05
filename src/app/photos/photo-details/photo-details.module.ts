e09f448e680142f423c4affeac9f70c08218181f
Juan Tavares <juantavares2006@hotmail.com>
2018-11-05 19:32:35 -0300

    Criando corpo de comentários

--- src/app/photos/photo-details/photo-details.module.ts
+++ src/app/photos/photo-details/photo-details.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { PhotoDetailsComponent } from './photo-details.component';
 import { PhotoModule } from '../photo/photo.module';
 import { PhotoCommentsComponent } from './photo-comments/photo-comments.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
-    PhotoModule
+    PhotoModule,
+    RouterModule
   ],
   declarations: [PhotoDetailsComponent, PhotoCommentsComponent],
   exports: [PhotoDetailsComponent]
