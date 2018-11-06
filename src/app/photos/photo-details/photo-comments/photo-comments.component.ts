0cb07be9c0dfb2803a9fec4ac45514c3f948b1c0
Juan Tavares <juantavares2006@hotmail.com>
2018-11-05 21:50:37 -0300

    Criando e aplicando método de submissão de comentários

--- src/app/photos/photo-details/photo-comments/photo-comments.component.ts
+++ src/app/photos/photo-details/photo-comments/photo-comments.component.ts
@@ -32,4 +32,14 @@ export class PhotoCommentsComponent implements OnInit {
     });
   }
 
+  save() {
+    const comment = this.commentForm.get('comment').value as string;
+
+    this.photoService
+      .addComment(this.photoId, comment)
+      .subscribe(() => {
+        this.commentForm.reset();
+      });
+  }
+
 }
