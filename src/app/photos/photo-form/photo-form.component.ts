dc23fc7f6fec5fdf5ab660614cca185f3577d6ed
Juan Tavares <juantavares2006@hotmail.com>
2018-11-09 20:18:51 -0300

    Aplicando alert para upload de photos

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { PhotoService } from '../photo/photo.service';
+import { AlertService } from 'src/app/shared/components/alert/alert.service';
 
 @Component({
   selector: 'app-photo-form',
@@ -16,7 +17,9 @@ export class PhotoFormComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private photoService: PhotoService,
-    private router: Router) { }
+    private router: Router,
+    private alertService: AlertService
+  ) { }
 
   ngOnInit() {
     this.photoForm = this.formBuilder.group({
@@ -31,7 +34,10 @@ export class PhotoFormComponent implements OnInit {
     const allowComments = this.photoForm.get('allowComments').value;
     this.photoService
       .upload(description, allowComments, this.file)
-      .subscribe(() => this.router.navigate(['']));
+      .subscribe(() => {
+        this.alertService.success('Upload complete!');
+        this.router.navigate([''])
+      });
   }
 
   handleFile(file: File) {
