1b3d7f69755aaf5390e17d4c345541aeda1c4488
Juan Tavares <juantavares2006@hotmail.com>
2018-11-20 20:34:36 -0300

    Aplicando menu no app

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -8,13 +8,15 @@ import { RequestInterceptor } from './auth/request.interceptor';
 import { FooterComponent } from './footer/footer.component';
 import { AlertModule } from '../shared/components/alert/alert.module';
 import { LoadingModule } from '../shared/components/loading/loading.module';
+import { MenuModule } from '../shared/components/menu/menu.module';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
     AlertModule,
-    LoadingModule
+    LoadingModule,
+    MenuModule
   ],
   declarations: [HeaderComponent, FooterComponent],
   exports: [
--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-menu',
+  selector: 'ap-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
 
+  isShown = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggle() {
+    this.isShown = !this.isShown;
   }
 
 }
