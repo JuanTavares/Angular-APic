f110cd4a266762cd8823c9332b62f84db55db548
Juan Tavares <juantavares2006@hotmail.com>
2018-11-22 20:46:19 -0300

    Configurando ErrorHandler para ter informações de back-end

--- src/app/errors/global-error-handler/global-error-handler.ts
+++ src/app/errors/global-error-handler/global-error-handler.ts
@@ -1,8 +1,29 @@
-import { ErrorHandler } from '@angular/core';
+import { ErrorHandler, Injectable, Injector } from '@angular/core';
+import * as Stacktrace from 'stacktrace-js';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { UserService } from 'src/app/core/user/user.service';
 
-export class GlobalErrorHandler implements ErrorHandler{
+@Injectable()
+export class GlobalErrorHandler implements ErrorHandler {
+
+    constructor(private injector: Injector) { }
 
     handleError(error: any): void {
-        throw error;
+
+        const location = this.injector.get(LocationStrategy);
+        const userService = this.injector.get(UserService);
+
+        const url = location instanceof PathLocationStrategy ? location.path() : '';
+
+        const message = error.message ? error.message : error.toString();
+
+        Stacktrace
+            .fromError(error)
+            .then(stackFrames => {
+                const stackAsString = stackFrames
+                    .map(sf => sf.toString())
+                    .join('\n');
+                console.log({message, url, username: userService.getUsername(), stack: stackAsString});
+            });
     }
 }
